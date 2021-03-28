1918134ce5d046c3b1e77d25fc8d43c1459e5184
Savannaha <savwiley@yahoo.com>
2021-03-28 13:36:29 -0500

    refactored with hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,161 +1,109 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class Header extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      nameVal: "",
-      emailVal: "",
-      phoneVal: "",
-      dRoleVal: "",
-      blurbVal: "",
-      head: [],
-    };
-
-    this.changeHeadName = this.handleChange.bind(this, "nameVal");
-    this.changeHeadEmail = this.handleChange.bind(this, "emailVal");
-    this.changeHeadPhone = this.handleChange.bind(this, "phoneVal");
-    this.changeHeadRole = this.handleChange.bind(this, "dRoleVal");
-    this.changeHeadBlurb = this.handleChange.bind(this, "blurbVal");
-    this.handleHeadEdit = this.handleEdit.bind(this, "head");
-  }
-
-  //handles input change
-  handleChange = (keyName, e) => {
-    this.setState({
-      [keyName]: e.target.value,
-    });
-  };
+const Header = () => {
+  const [nameVal, setNameVal] = useState("");
+  const [emailVal, setEmailVal] = useState("");
+  const [phoneVal, setPhoneVal] = useState("");
+  const [dRoleVal, setDRoleVal] = useState("");
+  const [blurbVal, setBlurbVal] = useState("");
 
   //submits header info
-  subHead = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const subHead = (e) => {
+      const form = document.querySelector(".headForm");
+      const subBtn = document.querySelector(".headSub");
+      const edBtn = document.querySelector(".editHead");
 
-    const form = document.querySelector(".headForm");
-    const edBtn = document.querySelector(".editHead");
+      const nameInput = document.querySelector("#Name");
+      const emailInput = document.querySelector("#Email");
+      const phoneInput = document.querySelector("#Phone");
+      const roleInput = document.querySelector("#DesRole");
+      const blurbInput = document.querySelector("#Blurb");
 
-    this.setState(
-      {
-        head: [],
-      },
-      () => {
-        this.setState({
-          head: this.state.head.concat(
-            this.state.nameVal,
-            this.state.emailVal,
-            this.state.phoneVal,
-            this.state.dRoleVal,
-            this.state.blurbVal
-          ),
-        });
-      }
-    );
+      setNameVal(nameInput.value);
+      setEmailVal(emailInput.value);
+      setPhoneVal(phoneInput.value);
+      setDRoleVal(roleInput.value);
+      setBlurbVal(blurbInput.value);
 
-    form.style.display = "none";
-    edBtn.style.display = "block";
-  };
+      form.style.display = "none";
+      subBtn.style.display = "none";
+      edBtn.style.display = "block";
+    };
+
+    const btn = document.querySelector(".headSub");
+    btn.addEventListener("click", () => {
+      subHead(this);
+    });
+  });
 
   //handles editing
-  handleEdit = (type, e) => {
+  useEffect(() => {
     //grabs form
     const headForm = document.querySelector(".headForm");
+    const subBtn = document.querySelector(".headSub");
+    const editHead = document.querySelector(".editHead");
+    editHead.addEventListener("click", () => {
+      handleEdit();
+    });
 
-    //brings up header form
-    if (type === "head") {
-      e.target.style.display = "none";
+    const handleEdit = () => {
+      editHead.style.display = "none";
       headForm.style.display = "block";
-    }
-  };
-
-  render() {
-    const { head } = this.state;
-    return (
-      <div>
-        <form onSubmit={this.subHead} className="headForm">
-          <div>
-            <input
-              type="text"
-              id="Name"
-              onChange={this.changeHeadName}
-              value={this.state.nameVal}
-              required
-            />
-            <label htmlFor="Name">Full Name</label>
-          </div>
-
-          <div>
-            <input
-              type="email"
-              id="Email"
-              onChange={this.changeHeadEmail}
-              value={this.state.emailVal}
-              required
-            />
-            <label htmlFor="Email">name@email.com</label>
-          </div>
-
-          <div>
-            <input
-              type="tel"
-              id="Phone"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              onChange={this.changeHeadPhone}
-              value={this.state.phoneVal}
-              required
-            />
-            <label htmlFor="Phone">Ph. xxx-xxx-xxxx </label>
-          </div>
-
-          <div>
-            <input
-              type="text"
-              id="DesRole"
-              onChange={this.changeHeadRole}
-              value={this.state.dRoleVal}
-              required
-            />
-            <label htmlFor="DesRole">Desired Role </label>
-          </div>
-
-          <div>
-            <textarea
-              type="text"
-              id="Blurb"
-              onChange={this.changeHeadBlurb}
-              value={this.state.blurbVal}
-              required
-            />
-            <label htmlFor="Blurb">Tell us about yourself... </label>
-          </div>
-
-          <button type="submit">Submit</button>
-        </form>
-
-        <button className="editHead" onClick={this.handleHeadEdit}>
-          Edit Header
-        </button>
-
-        <HeaderDiv head={head} />
-      </div>
-    );
-  }
-}
-
-function HeaderDiv(props) {
-  const { head } = props;
+      subBtn.style.display = "block";
+    };
+  });
 
   return (
-    <div className="header">
-      <span id="name">{head[0]}</span>
-      <div id="headerSide">
-        <span id="email">{head[1]}</span>
-        <span id="phone">{head[2]}</span>
+    <div>
+      <form className="headForm">
+        <div>
+          <input type="text" className="input" id="Name" required />
+          <label htmlFor="Name">Full Name</label>
+        </div>
+
+        <div>
+          <input type="email" className="input" id="Email" required />
+          <label htmlFor="Email">name@email.com</label>
+        </div>
+
+        <div>
+          <input
+            type="tel"
+            className="input"
+            id="Phone"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            required
+          />
+          <label htmlFor="Phone">Ph. xxx-xxx-xxxx </label>
+        </div>
+
+        <div>
+          <input type="text" className="input" id="DesRole" required />
+          <label htmlFor="DesRole">Desired Role </label>
+        </div>
+
+        <div>
+          <textarea type="text" className="input" id="Blurb" required />
+          <label htmlFor="Blurb">Tell us about yourself... </label>
+        </div>
+      </form>
+
+      <button className="headSub">Submit</button>
+
+      <button className="editHead">Edit Header</button>
+
+      <div className="header">
+        <span id="name">{nameVal}</span>
+        <div id="headerSide">
+          <span id="email">{emailVal}</span>
+          <span id="phone">{phoneVal}</span>
+        </div>
+        <span id="desRole">{dRoleVal}</span>
+        <span id="blurb">{blurbVal}</span>
       </div>
-      <span id="desRole">{head[3]}</span>
-      <span id="blurb">{head[4]}</span>
     </div>
   );
-}
+};
 
 export default Header;
